aa56c0f619870faf1f382d89c262e48dbd104f66
MemoryFate <18142010171@sina.cn>
2024-06-27 16:25:22 +0800

    no message

--- src/js/router.js
+++ src/js/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Home from "@/views/Home.vue"
 import Maze from "@/views/Game/Maze.vue"
+import text2qrcode from "@/views/Tools/text2qrcode.vue"
 const routes = [
     { path: "/", component: Home },
     { path: "/Game/Maze", component: Maze },
+    { path: "/Tools/text2qrcode", component: text2qrcode },
 ]
 
 const router = createRouter({
---
+++ src/views/Tools/text2qrcode.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="content">
+        <form>
+            <label for="text">转码文字:</label><br />
+            <textarea name="" id="text" placeholder="请输入要转码的内容" v-model="data.text"></textarea>
+            <button type="submit">确定</button>
+        </form>
+        <canvas id="qrcode" height="600" width="600"></canvas>
+    </div>
+</template>
+<script setup>
+import { getCurrentInstance, onMounted, reactive } from "vue"
+const data = reactive({
+    text: "",
+})
+const { proxy, emit } = getCurrentInstance()
+</script>
+<style lang="scss" scoped>
+.content {
+    display: flex;
+    flex-direction: column;
+}
+</style>
