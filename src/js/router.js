6b470e69b5152efc45d0dc5d4983f8780a1a82b4
mulei <mulei_ah@bonc.com.cn>
2025-05-27 12:02:51 +0800

    no message

--- src/js/router.js
+++ src/js/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
     { path: "/index", component: () => import("@/views/Home.vue"), name: "home" },
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from "node:url"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    // base: '/raym-blog/',
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -12,5 +13,4 @@ export default defineConfig({
     server: {
         port: 8080,
     },
-    base: '/raym-blog/'
 })
