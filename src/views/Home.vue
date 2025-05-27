590fa80451cc1c8f8fb6652a8f653b4cd9579313
mulei <mulei_ah@bonc.com.cn>
2025-05-27 11:11:10 +0800

    no message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-    
+  <div style="display: flex; flex-direction: row; flex-wrap: wrap; gap: 20px">
+    <a @click="navTo(item)" v-for="(item, index) in data.menus" :key="index" style="width: 200px">
+        <a-card :title="item.title" :body-style="{height: '100px', overflow: 'hidden', whiteSpace: 'wrap', textOverflow: 'ellipsis'}">
+            {{ item.content }}
+        </a-card>
+    </a>
   </div>
 </template>
 
 <script setup>
-
+import { reactive, getCurrentInstance } from "vue"
+const { proxy } = getCurrentInstance()
+const data = reactive({
+    menus: [
+        {
+            title: "Tools",
+            content: "工具类",
+            path: "/Tools",
+        },
+        {
+            title: "Game",
+            content: "游戏",
+            path: "/Game",
+        },
+    ],
+})
+function navTo(item) {
+    console.log(proxy.$router)
+    proxy.$router.push({ path: item.path })
+}
 </script>
 
 <style scoped>
