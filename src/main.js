98a44303910d0dad4de52dd3d75ffed96b10128f
XXXX <596104348@qq.com>
2026-09-19 02:19:14 +0800

    feat: wire redesigned site shell

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue"
 import router from "./js/router.js"
 import Antd from "ant-design-vue"
 import "ant-design-vue/dist/reset.css"
+import "@/assets/styles/theme.scss"
 
 const app = createApp(App)
 app.use(router)
