a6b5ea83c42f6bacebb15a53ff52f05fcbc04efc
XXXX <596104348@qq.com>
2026-09-19 02:19:16 +0800

    feat: wire redesigned site shell

--- src/js/router.js
+++ src/js/router.js
@@ -21,6 +21,11 @@ const routes = [
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) return savedPosition
+        if (to.hash) return { el: to.hash, top: 112, behavior: "smooth" }
+        return { top: 0 }
+    },
 })
 
 export default router
