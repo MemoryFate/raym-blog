9bbbb5cd347420faae747385dd7e93d9be514e0d
XXXX <596104348@qq.com>
2026-09-19 11:57:36 +0800

    feat: enable shared detail control system

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./js/router.js"
 import Antd from "ant-design-vue"
 import "ant-design-vue/dist/reset.css"
 import "@/assets/styles/theme.scss"
+import "@/assets/styles/control-system.scss"
 
 const app = createApp(App)
 app.use(router)
